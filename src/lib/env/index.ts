a9f39e6d9b0d34574a80aa18ca133db94f02b752
Nirvik Purkait <nirvikpurkait@gmail.com>
2026-02-17 16:43:38 +0530

    env var filtered

--- src/lib/env/index.ts
+++ src/lib/env/index.ts
@@ -4,10 +4,10 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string(),
-    GOOGLE_CLIENT_ID: z.string(),
-    GOOGLE_CLIENT_SECRET: z.string(),
-    BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.string(),
+    // GOOGLE_CLIENT_ID: z.string(),
+    // GOOGLE_CLIENT_SECRET: z.string(),
+    // BETTER_AUTH_SECRET: z.string(),
+    // BETTER_AUTH_URL: z.string(),
   },
 
   /**
